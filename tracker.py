d7d701c5699372d35bbc36f1289d28bb27afa428
Your Name <tbowetumawe@myges.fr>
2022-07-20 23:07:10 +0200

    finale

--- tracker.py
+++ tracker.py
@@ -2,24 +2,24 @@ import math
 
 class Trackers:
     def __init__(self):
-        # Store the center positions of the objects
+        # Stocker les positions centrales des objets
         self.center_points = {}
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
+
+        # chaque fois qu'un nouvel objet est détecté, le count est incrementé
         self.id_count = 0
 
 
     def update(self, objects_rect):
-        # Objects boxes and ids
+
         objects_bbs_ids = []
 
-        # Get center point of new object
+        # Obtenir le point central d'un nouvel objet
         for rect in objects_rect:
             x, y, w, h, index = rect
             cx = (x + x + w) // 2
             cy = (y + y + h) // 2
 
-            # Find out if that object was detected already
+            # Vérifier si cet objet a déjà été détecté
             same_object_detected = False
             for id, pt in self.center_points.items():
                 dist = math.hypot(cx - pt[0], cy - pt[1])
@@ -31,19 +31,19 @@ class Trackers:
                     same_object_detected = True
                     break
 
-            # New object is detected we assign the ID to that object
+            # Un nouvel objet est détecté, nous attribuons l'ID à cet objet.
             if same_object_detected is False:
                 self.center_points[self.id_count] = (cx, cy)
                 objects_bbs_ids.append([x, y, w, h, self.id_count, index])
                 self.id_count += 1
 
-        # Clean the dictionary by center points to remove IDS not used anymore
+        # Nettoyer le dictionnaire par points centraux pour supprimer les IDS qui ne sont plus utilisés
         new_center_points = {}
         for obj_bb_id in objects_bbs_ids:
             _, _, _, _, object_id, index = obj_bb_id
             center = self.center_points[object_id]
             new_center_points[object_id] = center
 
-        # Update dictionary with IDs not used removed
+        # Mise à jour du dictionnaire avec les IDs non utilisés supprimés
         self.center_points = new_center_points.copy()
         return objects_bbs_ids
--- vehicle_count.py
+++ vehicle_count.py
@@ -5,6 +5,7 @@ import cv2
 import csv
 import os
 import boto3
+import moviepy.editor as moviepy
 import collections
 import numpy as np
 from botocore.exceptions import ClientError
@@ -12,14 +13,13 @@ from matplotlib import pyplot as plt
 
 from tracker import *
 
-# Initialize Tracker
+# Tracker
 tracker = Trackers()
 
-# Initialize the videocapture object
 
 input_size = 255
 
-# Detection confidence threshold
+
 confThreshold =0.2
 nmsThreshold= 0.2
 
@@ -27,41 +27,41 @@ font_color = (0, 0, 255)
 font_size = 0.5
 font_thickness = 2
 
-# Middle cross line position
+
 middle_line_position = 255
 up_line_position = middle_line_position - 15
 down_line_position = middle_line_position + 15
 
 
-# Store Coco Names in a list
+#  Coco Names
 classesFile = "coco.names"
 classNames = open(classesFile).read().strip().split('\n')
 print(classNames)
 print(len(classNames))
 
-# class index for our required detection classes
+# indice de classe pour nos classes de détection requises
 required_class_index = [2, 3, 5, 7]
 
 detected_classNames = []
 
-## Model Files
+## Modeles
 modelConfiguration = 'C:/Users/bowet/Downloads/vehicle-detection/models/yolov4.cfg'
 modelWeigheights = 'C:/Users/bowet/Downloads/vehicle-detection/models/yolov4.weights'
 
-# configure the network model
+
 net = cv2.dnn.readNet(modelConfiguration, modelWeigheights)
-# Configure the network backend
+
 
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-# Define random colour for each class
+
 np.random.seed(42)
 colors = np.random.randint(0, 255, size=(len(classNames), 3), dtype='uint8')
 
 
 
-# Function for finding the center of a rectangle
+#
 def find_center(x, y, w, h):
     x1=int(w/2)
     y1=int(h/2)
@@ -69,22 +69,22 @@ def find_center(x, y, w, h):
     cy=y+y1
     return cx, cy
     
-# List for store vehicle count information
+
 temp_up_list = []
 temp_down_list = []
 up_list = [0, 0, 0, 0]
 down_list = [0, 0, 0, 0]
 
-# Function for count vehicle
+
 def count_vehicle(box_id, img):
 
     x, y, w, h, id, index = box_id
 
-    # Find the center of the rectangle for detection
+
     center = find_center(x, y, w, h)
     ix, iy = center
     
-    # Find the current position of the vehicle
+    # position du vehicule
     if (iy > up_line_position) and (iy < middle_line_position):
 
         if id not in temp_up_list:
@@ -104,13 +104,13 @@ def count_vehicle(box_id, img):
             temp_up_list.remove(id)
             down_list[index] = down_list[index] + 1
 
-    # Draw circle in the middle of the rectangle
-    cv2.circle(img, center, 2, (0, 0, 255), -1)  # end here
+
+    cv2.circle(img, center, 2, (0, 0, 255), -1)
     # print(up_list, down_list)
 
 
-# Function for finding the detected objects from the network output
-def postProcess(outputs,img):
+# Fonction permettant de retrouver les objets détectés à partir du network output
+def Processing(outputs,img):
 
     global detected_classNames 
     height, width = img.shape[:2]
@@ -136,7 +136,7 @@ def postProcess(outputs,img):
                     confidence_scores.append(float(confidence))
 
 
-    # Apply Non-Max Suppression
+
     indices = cv2.dnn.NMSBoxes(boxes, confidence_scores, confThreshold, nmsThreshold)
     # print(classIds)
     items = []
@@ -145,22 +145,22 @@ def postProcess(outputs,img):
             x, y, w, h = boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]
             # print(x,y,w,h)
 
-            # To get the name of object
+            # recuperer le label de chaque objet
             label = str.upper((classNames[classIds[i]]))
 
             color = [int(c) for c in colors[classIds[i]]]
             name = classNames[classIds[i]]
             detected_classNames.append(name)
-            # Draw classname and confidence score
+            # Dessinez le nom de la classe et le score de confiance
             cv2.putText(img,f'{name.upper()} {int(confidence_scores[i]*100)}%',
                       (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
 
-            # Draw bounding rectangle
+            # Tracer le rectangle de délimitation
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 1)
             items.append(label)
             detection.append([x, y, w, h, required_class_index.index(classIds[i])])
 
-    # Update the tracker for each object
+    #Mettre à jour le tracker pour chaque objet
 
     boxes_ids = tracker.update(detection)
     for box_id in boxes_ids:
@@ -186,23 +186,23 @@ def realTime(img, cap):
 
         blob = cv2.dnn.blobFromImage(img, 1 / 255, (input_size, input_size), [0, 0, 0], 1, crop=False)
 
-        # Set the input of the network
+
         net.setInput(blob)
         layersNames = net.getLayerNames()
         outputNames = [layersNames[i - 1] for i in net.getUnconnectedOutLayers()]
-        # Feed data to the network
+        # Alimenter network
         outputs = net.forward(outputNames)
     
         # Find the objects from the network output
-        postProcess(outputs,img)
+        Processing(outputs, img)
 
-        # Draw the crossing lines
+        # Tracez les lignes de croisement
 
         cv2.line(img, (0, middle_line_position), (iw, middle_line_position), (255, 0, 255), 2)
         cv2.line(img, (0, up_line_position), (iw, up_line_position), (0, 0, 255), 2)
         cv2.line(img, (0, down_line_position), (iw, down_line_position), (0, 0, 255), 2)
 
-        # Draw counting texts in the frame
+        # Dessinez des textes de comptage dans le cadre
         cv2.putText(img, "Up", (110, 20), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
         cv2.putText(img, "Down", (160, 20), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
         cv2.putText(img, "Car:        "+str(up_list[0])+"     "+ str(down_list[0]), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
@@ -213,7 +213,7 @@ def realTime(img, cap):
 
         out.write(img)
 
-        # Show the frames
+
         cv2.imshow('Output', img)
 
         if cv2.waitKey(1) == ord('q'):
@@ -236,9 +236,9 @@ def realTime(img, cap):
     access_key = 'AKIASN67I6KKE2L4ZTVI'
     secret_key = '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'
     bucket_name = 'projeta'
-    """
-    Connexion s3
-    """
+
+    # Connexion s3
+
     client_s3 = boto3.client(
         service_name='s3',
         region_name="eu-west-3",
@@ -246,9 +246,9 @@ def realTime(img, cap):
         aws_secret_access_key=secret_key
     )
     # bucket = client_s3.Bucket(bucket_name)
-    """
-       chargement de fichier dans le compartiment s3
-    """
+
+    # transfer du fichier dans le compartiment s3
+
     data_folder = r'C:/Users/bowet/Downloads/vehicle-detection/data/ouput/'
 
     for file in os.listdir(data_folder):
@@ -266,7 +266,7 @@ def realTime(img, cap):
             except Exception as e:
                 print(e)
 
-    # Finally realese the capture object and destroy all active windows
+
     cap.release()
     out.release()
     cv2.destroyAllWindows()
@@ -274,7 +274,7 @@ def realTime(img, cap):
 
 
 def object_main():
-    """OBJECT DETECTION APP"""
+
 
     st.title("Flux traffic")
     st.write("Dans ce projet, nous allons détecter et suivre les voitures sur la route, et compter le nombre de véhicules circulant sur une route. Et les données seront stockées pour analyser différents véhicules qui circulent sur la route.")
@@ -285,36 +285,49 @@ def object_main():
     demo_video = 'data/Road.mp4'
     tffile = tempfile.NamedTemporaryFile(suffix='.mp4', delete=False)
 
-    if video_file_buffer:
+    if video_file_buffer != None:
+        vid = video_file_buffer.name
+        with open(vid, mode='wb') as f:
+            f.write(video_file_buffer.read())  # save video to disk
 
-        tffile.write(video_file_buffer.read())
-        demo_vid = open(tffile.name, 'rb')
-        read_demo = demo_vid.read()
-        vid = cv2.VideoCapture(read_demo)
-        st.text("vidéo d'entré")
-        st.video(read_demo)
-        realTime(read_demo, vid)
+        st_video = open(vid, 'rb')
+        video_bytes = st_video.read()
+        st.video(video_bytes)
+        st.write("Uploaded Video")
+        # video_file = 'street.mp4'
+        cap = cv2.VideoCapture(vid)
 
-        tffile.name = 'data/ouput/detected_video.mp4'
-        output_vid = open(tffile.name, 'rb')
-        read_output = output_vid.read()
-        st.text("vidéo de sotie")
-        st.video(read_output)
-    # else:
-    #     vid = cv2.VideoCapture(demo_video)
-    #     tffile.name = demo_video
-    #     demo_vid = open(tffile.name, 'rb')
-    #     read_demo = demo_vid.read()
-    #     st.text("vidéo d'entré")
-    #     st.video(read_demo)
-    #     realTime(demo_video, vid)
-    #
-    #     tffile.name ='data/ouput/detected_video.mp4'
-    #     output_vid = open(tffile.name, 'rb')
-    #     read_output = output_vid.read()
-    #     st.text("vidéo de sotie")
-    #     st.video(read_output)
+        realTime(vid, cap)
+        try:
+            clip = moviepy.VideoFileClip('data/ouput/detected_video.mp4')
+            clip.write_videofile("myvideo.mp4")
+            st_video = open('myvideo.mp4', 'rb')
+            video_bytes = st_video.read()
+            st.video(video_bytes)
+            st.write("Detected Video")
 
+            # tffile.name = 'data/ouput/detected_video.mp4'
+            # output_vid = open(tffile.name, 'rb')
+            # read_output = output_vid.read()
+            # st.text("vidéo de sotie")
+            # st.video(read_output)
+
+            # else:
+            #     vid = cv2.VideoCapture(demo_video)
+            #     tffile.name = demo_video
+            #     demo_vid = open(tffile.name, 'rb')
+            #     read_demo = demo_vid.read()
+            #     st.text("vidéo d'entré")
+            #     st.video(read_demo)
+            #     realTime(demo_video, vid)
+            #
+            #     tffile.name ='data/ouput/detected_video.mp4'
+            #     output_vid = open(tffile.name, 'rb')
+            #     read_output = output_vid.read()
+            #     st.text("vidéo de sotie")
+            #     st.video(read_output)
+        except OSError:
+            'e'
 
 
 if __name__ == '__main__':
